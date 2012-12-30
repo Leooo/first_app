aeec642262b14312d4b4e63eda8325d293d42ee3
Leooo <viet_lionel@yahoo.fr>
2012-12-30 16:16:29 +0000

    End chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -46,14 +47,8 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+  private
 
     def correct_user
       @user = User.find(params[:id])
